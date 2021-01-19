af311586a48609de8d6ef5d2a2ebeacaf16f5673
targrimus <targrimus@gmail.com>
2021-01-19 22:35:21 +0300

    db connection

--- bin/app.js
+++ bin/app.js
@@ -2,13 +2,19 @@
 const express = require("express");
 const expbs = require("express-handlebars");
 const path = require("path");
+const mongoose = require("mongoose");
+const { promises } = require("fs");
+
+require("dotenv").config();
+
+
 
 // application starts here
 const app = express();
 
 // other constants
-const __basedir = process.cwd();
 const port = 3000 | process.env.PORT;
+const __basedir = process.cwd();
 
 // Sending static files with Express 
 app.use(express.static(path.join(__basedir,'public')));
@@ -46,9 +52,21 @@ const routes = require(path.join(__basedir,"routes/router.js"));
 app.use(routes);
 
 
-// start server
-app.listen(port, (err)=>{
-    if (err) throw err;
-    console.log(`app is running on port ${port}`);
-})
+
+//db connection and timer
+const timer = Date.now();
+mongoose.connect(process.env.DB_URI, { 
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex:true })
+    .then((result)=>{
+        console.log("connection timer: ", Date.now()-timer," miliseconds");
+        app.listen(port,(error)=>{
+            console.log("App started on port", port);
+        })
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
+
 
