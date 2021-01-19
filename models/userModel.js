d7adbcb96d223ecbb3144e0ae44959cc917ac491
targrimus <targrimus@gmail.com>
2021-01-19 21:46:22 +0300

    new commit

--- models/userModel.js
+++ models/userModel.js
@@ -1,1 +1,27 @@
 const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    surname: {
+        type: String,
+        required: true,
+        lowercase: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    }
+  });
+
+  module.exports = mongoose.model('User', userSchema);
